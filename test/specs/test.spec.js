c8ababfe2cddfc7c383d06be09e5066abf99c003
Mamuka Jincharadze <mamuka_jincharadze@yahoo.com>
2022-09-12 23:12:30 +0400

    test 2 completed

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -14,5 +14,28 @@ describe("test", () => {
         await browser.url(webPage);
     });
 
+    // 2. Create a New Paste with the following details:
+    // * Code:
+    // git config --global user.name "New Sheriff in Town"
+    // git reset $ (git commit-tree HEAD ^ {tree} -m "Legacy code")
+    // git push origin master --force
+    // * Syntax Highlighting: "Bash"
+    // * Paste Expiration: "10 Minutes"
+    // * Paste Name / Title: "how to gain dominance among developers"
+
+    it("test2", async () => {
+
+        await $("#postform-text").setValue(pasteText);
+
+        await $("#select2-postform-format-container").click();
+        await $("/html/body/span[2]/span/span[2]/ul/li[2]/ul/li[1]").click();
+
+        await $("#select2-postform-expiration-container").click();
+        await $("/html/body/span[2]/span/span[2]/ul/li[3]").click();
+
+        await $("#postform-name").setValue(pasteName);
+
+        await $("div.form-group.form-btn-container > button").click();
+    });
 
 })
